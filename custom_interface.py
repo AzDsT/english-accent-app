7ff149965b993325044d602e42febf94f45449ea
Turgut Guliyev <91908387+AzDsT@users.noreply.github.com>
2025-05-24 17:39:24 +0400

    Update custom_interface.py

--- custom_interface.py
+++ custom_interface.py
@@ -1,9 +1,6 @@
 from speechbrain.inference.interfaces import Pretrained
 import torch
 
-from speechbrain.pretrained.interfaces import foreign_class
-
-
 def classify_accent(audio_path):
     out_prob, score, index, label = classifier.classify_file(audio_path)
     return label[0], round(float(score[0]), 3)
